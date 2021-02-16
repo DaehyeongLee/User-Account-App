f570d0ea38ab5fea50c4b4910ff151621379801d
daelee <daelee@bizflow.com>
2021-02-16 23:56:56 +0900

    Logout 기능 추가

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-const {User} = require('../models/User');
+const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
 
     //인증 처리를 하는 곳
 
     //클라이언트 쿠키에서 토큰을 가져온다.
-    let token = req.cookie.x_auth;
+    let token = req.cookies.x_auth;
 
     //토큰을 복호화한 후 유저를 찾는다
     User.findByToken(token, (err, user) => {
