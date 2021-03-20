0e1d4f292f79222b4abba90f3ae1a97a3df207e6
daelee <daelee@bizflow.com>
2021-03-21 00:40:59 +0900

    Redux explanation

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css'; //CSS 프레임워크 npm install antd --save
+
 //여기 안에 보여지고 싶은 컴포넌트를 넣는다
 //concurrently는? client server와 back server를 같이 켜주기 위함
 //-> "dev": "concurrently \"npm run nodemon\" \"npm run start --prefix client\""
@@ -18,3 +20,20 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//Props vs State
+//Props: properties. 부모 컴퍼넌트안에 자식 컴퍼넌트가 있다. 부모에서 자녀로만 데이터를 보낼 수 있다.
+//Props에서 부모에서 자식으로 내려줬을 때 자식에서 받은 값을 변경할 순 없다.
+//State: 컴퍼넌트 안에서 데이터를 교환, 전달할 때 state 사용.
+//state는 mutable. Data는 re-rendering으로 변경 가능
+//EX) Props
+//  <ChatMessages
+//    messages = {messages}
+//  />
+//EX) State
+//  state = {
+//    message: ''      
+//  } 
+
+//Redux: Store를 통해 state를 감싸주어 관리
